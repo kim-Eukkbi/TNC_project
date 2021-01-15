1e86058133131a052cf4ea454583ef0f731fe331
김성후 <smbaekr56@naver.com>
2021-01-15 16:57:49 +0900

    대사 한글자씩 나오는거 만들었는데 아직 버튼이나 다른것은 구현 안함

---
+++ Assets/Scripts/ChatCtrl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChatCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private Text ChatText;
+    [SerializeField]
+    private Text CharacterName;
+    [SerializeField]
+    private float TextDelay;
+
+    private void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private IEnumerator NormalChat(string narrator , string narration)
+    {
+        int i = 0;
+        CharacterName.text = narrator;
+        string letter = "";
+
+        for(i =0; i<narration.Length;i++)
+        {
+            letter += narration[i];
+            ChatText.text = letter;
+            yield return new WaitForSeconds(TextDelay);
+        }
+    }
+
+    private IEnumerator ShowText()
+    {
+        yield return StartCoroutine(NormalChat("대이", "파괴파괴파괴파괴파괴파괴파괴파괴파괴파괴파괴파괴파괴파괴파괴파괴"));
+    }
+}
