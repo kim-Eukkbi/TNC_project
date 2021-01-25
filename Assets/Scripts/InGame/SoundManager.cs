c31bc01811ad9f2062cd00b57a0997c60c6d86fd
K_beomju <kbj52264291@gmail.com>
2021-01-25 23:23:28 +0900

    TEST

---
+++ Assets/Scripts/InGame/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource backGroundmusic;
+    public AudioSource soundEffect;
+
+    public void SetbackVolume(float volume)
+    {
+        backGroundmusic.volume = volume;
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        soundEffect.volume = volume;
+    }
+
+    public void OnSfx()
+    {
+        soundEffect.Play();
+    }
+}
